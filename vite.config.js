8199cdfb12daf9f573eaa813f909558fc5f00a26
ArmanMarchiel <arman.marchiel23@gmail.com>
2025-02-08 16:54:53 -0700

    Simplified GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/CS5702_FinalProject/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  // Explicitly copy public files
-  publicDir: 'public'
 })
