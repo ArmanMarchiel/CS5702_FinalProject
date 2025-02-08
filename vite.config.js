0f59d84baad144ebcf12b71e1e6666fdc25a28c0
ArmanMarchiel <arman.marchiel23@gmail.com>
2025-02-08 16:43:11 -0700

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CS5702_FinalProject/',
-  server: {
-    port: 3000,
-    open: true
-  }
+  base: '/CS5702_FinalProject/'
 })
